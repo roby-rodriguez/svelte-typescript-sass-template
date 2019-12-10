6b76728b0a14029a161f3f8c58333bc5532b0f2e
Andrei Roba <Andrei.Roba@msg.group>
2019-12-10 10:08:58 +0200

    first (working) draft

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<template type="text/pug">
+    h1 Hello!
+    br
+    input(bind:value="{label}")
+    br
+    Nested("{label}")
+</template>
+
+<script type="text/typescript">
+    import Nested from './Nested.svelte'
+    export let label: string = ''
+    $: console.log(label)
+</script>
+
+<style type="text/stylus">
+    h1
+        color blue
+</style>
